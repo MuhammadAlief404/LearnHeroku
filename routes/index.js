e794e0f8f8840e431095eef2af93f767ced56fe4
prk <asda@adasdas>
2018-12-13 12:36:41 +0700

    first add

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+router.use(passport.initialize());
+
+passport.use(new FacebookStrategy({
+  clientID : '200001347550259',
+  clientSecret : 'caa313cd970bee7bb5828be8b7d5a15f',
+  callbackURL: 'https://8e796e02.ap.ngrok.io/authFacebook/done',
+  profileFields: ['id', 'name', 'email', 'photos']
+}, function(accessToken, refreshToken, profile, done){
+  return done(null, profile);
+}))
+
+passport.serializeUser(function(profile,done){
+  return done(null, profile);
+})
+
+passport.deserializeUser(function(profile,done){
+  return done(null, profile);
+})
+
+router.get('/authFacebook', passport.authenticate('facebook'));
+router.get('/authFacebook/done', 
+passport.authenticate('facebook', {
+  failureRedirect: '/'
+}),function(req,res){
+  return res.json(req.user);
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
